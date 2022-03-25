dc2bd5f6dcdfea91f1367e8c27f8c06ad4ab8d8e
christopher <christobelstat@gmail.com>
2022-03-25 07:09:15 -0700

    selectableCommit

--- database.py
+++ database.py
@@ -18,13 +18,26 @@ def select_todos():
     todos = [entry[0] for entry in cursor.fetchall()]
 
     return todos
+
+
+def select_table():
+    request = "SELECT * FROM data_beers;"
+    cursor.execute(request)
+
+    table = [entry[0] for entry in cursor.fetchall()]
+
+    return table
+
+
 def create_pwd():
     request = "CREATE TABLE Pwd(pseudo VARCHAR(100),motdepasse VARCHAR(100),PRIMARY KEY (pseudo))"
     cursor.execute(request)
 
+
 def insert_pwd(pseudo,motdepasse):
     request = """INSERT INTO pwd (pseudo,motdepasse) VALUES ("{}","{}");""".format(pseudo,motdepasse)
     cursor.execute(request)
+
 
 def verif_mdp(username,mdp):
     request= "SELECT pseudo, motdepasse FROM pwd;"
@@ -32,12 +45,14 @@ def verif_mdp(username,mdp):
 
     list_mdp = [(entry[0],entry[1]) for entry in cursor.fetchall()]
 
+
     #for entry in cursor.fetchall():
        # if username==entry[0] and mdp==entry[1]:
        #     return print("identite_confirme")
      #   else :
      #       return print("Compte inconnu")
     return list_mdp
+
 
 def delete_pwd():
     request = "DROP TABLE Pwd"
